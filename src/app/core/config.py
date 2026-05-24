dcbcc38bbe690d662f6b75da271afb4e5f725a02
Maniakalochka <prochorwolochoff@gmail.com>
2026-05-24 18:45:17 +0300

    Add configuration settings for PostgreSQL database

---
+++ src/app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # Relation Database settings
+    POSTGRES_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+settings = Settings()  # type: ignore
