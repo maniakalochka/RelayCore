e9d2e696aac47325d0549c6a016c38acb0230e2d
Maniakalochka <prochorwolochoff@gmail.com>
2026-06-16 10:28:13 +0300

    Add JWT authentication backend and user management functionality

---
+++ src/app/auth/deps.py
@@ -0,0 +1,28 @@
+import uuid
+from collections.abc import AsyncIterator
+
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.strategies import CustomJWTStrategy
+from app.core.config import settings
+from app.db.deps import get_async_session
+from app.models.user import User
+
+
+async def get_user_db(
+    session: AsyncSession = Depends(get_async_session),
+) -> AsyncIterator[SQLAlchemyUserDatabase[User, uuid.UUID]]:  # type: ignore[type-var]
+    yield SQLAlchemyUserDatabase(session, User)  # type: ignore[type-var]
+
+
+def get_jwt_strategy() -> CustomJWTStrategy:
+    return CustomJWTStrategy(
+        secret=settings.JWT_SECRET,
+        lifetime_seconds=settings.ACCESS_TOKEN_LIFETIME_S,
+        token_audience=[
+            "api",
+        ],
+        algorithm="HS256",
+    )
