3b2aceabbde28e84225683906aeec011389d61be
Maniakalochka <prochorwolochoff@gmail.com>
2026-06-16 10:23:26 +0300

    Add async database session management and base model

---
+++ src/app/db/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from app.core.config import settings
+
+DATABASE_URL = settings.POSTGRES_URL
+DATABASE_PARAMS = {
+    "poolclass": NullPool,
+}
+
+engine = create_async_engine(url=DATABASE_URL, echo=False, **DATABASE_PARAMS)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
